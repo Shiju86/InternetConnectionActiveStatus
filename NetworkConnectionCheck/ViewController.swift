aed823ecf2d751980a84b3a4576222ac8b590216
Shiju Varghese <shiju_varghese@apple.com>
2020-07-06 01:00:04 +0530

    Initial Commit

---
+++ NetworkConnectionCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkConnectionCheck
+//
+//  Created by Shiju Varghese on 06/07/20.
+//  Copyright © 2020 Shiju Varghese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
