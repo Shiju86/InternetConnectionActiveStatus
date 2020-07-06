8a34623f29990b00ad066f69df2b9a56b34b3858
Shiju Varghese <shiju_varghese@apple.com>
2020-07-06 13:15:41 +0530

    Check Internet connection status

--- NetworkConnectionCheck/ViewController.swift
+++ NetworkConnectionCheck/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import Network
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkInternetConnectStatus()
     }
-
-
+    
+    func checkInternetConnectStatus() {
+        
+        let pathMonitor = NWPathMonitor()
+        pathMonitor.pathUpdateHandler = { path in
+           
+            if path.status == .satisfied {
+                
+                DispatchQueue.main.async {
+                    self.view.backgroundColor = UIColor.green
+                }
+                print("Active Internet Connection")
+            }
+            else{
+                DispatchQueue.main.async {
+                    self.view.backgroundColor = UIColor.red
+                }
+                print("No Internet Connection")
+            }
+        }
+        
+        let queue = DispatchQueue(label: "Network")
+        pathMonitor.start(queue: queue)
+    }
 }
 
